58e4d7278d089dad3ba86019ef449043d5464263
goobdevTM <getfreegame631@gmail.com>
2026-01-14 21:40:51 -0700

    r6i65y
    6yth

--- glambert's-largely-sized-adventures-in-glam-land™!/scripts/glambert.gd
+++ glambert's-largely-sized-adventures-in-glam-land™!/scripts/glambert.gd
@@ -4,14 +4,17 @@ class_name Glambert
 
 const GROUND_FRICTION: float = 0.9
 const AIR_FRICTION: float = 0.98
-const MAX_COYOTE_TIME: float = 0.5
+const MAX_COYOTE_TIME: float = 0.08
+const MAX_BUFFER_JUMP: float = 0.08
 const MAX_JUMPS: int = 2
 
 var coyote_time: float = MAX_COYOTE_TIME
+var buffer_jump: float = MAX_BUFFER_JUMP
 var speed: float = 1200.0
 var jump_velocity: float = -300.0
 var friction: float = 0.9
 var jumps: int = MAX_JUMPS
+var fall_time : float = 0
 
 var flipped: bool = false
 var current_animation: String
@@ -19,9 +22,10 @@ var in_air: bool = false
 
 
 #SPRITES
-@onready var sprite_facing_left: AnimatedSprite2D = $Sprite/SpriteFacingLeft
-@onready var sprite_facing_right: AnimatedSprite2D = $Sprite/SpriteFacingRight
-@onready var glambert_sunglasses: Sprite2D = $Sprite/GlambertSunglasses
+@onready var sprite_facing_left: AnimatedSprite2D = $Model/Sprites/SpriteFacingLeft
+@onready var sprite_facing_right: AnimatedSprite2D = $Model/Sprites/SpriteFacingRight
+@onready var sprites: Node2D = $Model/Sprites
+@onready var glambert_sunglasses: Sprite2D = $Model/Sprites/GlambertSunglasses
 @onready var smooth_animations: AnimationPlayer = $SmoothAnimations
 #SOUNDS
 @onready var boing: AudioStreamPlayer = $Boing
@@ -29,7 +33,7 @@ var in_air: bool = false
 @onready var camera: Camera2D = $"../Camera"
 
 func _ready() -> void:
-
+	buffer_jump = 0
 	jumps = MAX_JUMPS
 	coyote_time = MAX_COYOTE_TIME
 	friction = GROUND_FRICTION
@@ -41,29 +45,36 @@ func _physics_process(delta: float) -> void:
 	
 	if is_on_floor():
 		in_air = false
+		fall_time = 0
 	else:
 		in_air = true
+		fall_time += delta
 	
 	if in_air:
 		velocity += get_gravity() * delta
-		coyote_time -= delta
 	else:
 		jumps = MAX_JUMPS
 		coyote_time = MAX_COYOTE_TIME
 		in_air = false
 		
+	#SUBTRACT VARIABLES
+	coyote_time -= delta
+	buffer_jump -= delta
+		
 
 	# Handle jump.
 	
-
-	if jumps > 0 and (Input.is_action_just_pressed("jump") and coyote_time > 0):
-		coyote_time = MAX_COYOTE_TIME
-		velocity.y = jump_velocity
+	if Input.is_action_just_pressed("jump"):
+		buffer_jump = MAX_BUFFER_JUMP
+		
+	if jumps > 0 and buffer_jump > 0:
+		fall_time = 0
 		boing.play()
-		if not in_air:
+		if coyote_time <= 0:
 			jumps -= 1
-		else:
-			jumps = 0
+		coyote_time = MAX_COYOTE_TIME
+		buffer_jump = 0
+		velocity.y = jump_velocity
 	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -90,6 +101,7 @@ func _physics_process(delta: float) -> void:
 	camera.global_position = lerp(camera.global_position, global_position, 0.08)
 	
 	set_animation(current_animation)
+	falling_animation(delta)
 	move_and_slide()
 	
 
@@ -108,3 +120,10 @@ func set_animation(animation: String):
 	sprite_facing_left.play(animation)
 	sprite_facing_right.play(animation)
 	smooth_animations.play(animation)
+	
+func falling_animation(delta : float) -> void:
+	if in_air:
+		fall_time = clamp(fall_time, 0, 1)
+		sprites.scale = lerp(Vector2(1.0,1.0), Vector2(0.7,1.3), fall_time)
+	else:
+		sprites.scale = lerp(sprites.scale, Vector2(1,1), delta * 30)
