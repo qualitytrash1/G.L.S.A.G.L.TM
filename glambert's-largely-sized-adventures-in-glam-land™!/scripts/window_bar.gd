d48f55c2b5469b8375baf6bcafba73f0de42ffd4
goobdevTM <getfreegame631@gmail.com>
2026-02-04 21:45:10 -0700

    TILET BAR

--- glambert's-largely-sized-adventures-in-glam-land™!/scripts/window_bar.gd
+++ glambert's-largely-sized-adventures-in-glam-land™!/scripts/window_bar.gd
@@ -4,6 +4,9 @@ extends CanvasLayer
 
 class_name WindowBar
 
+@onready var bar: Control = $Bar
+
+
 var mouse_hovering : bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -35,25 +38,38 @@ func check_visibility() -> void:
 
 
 func _on_window_bar_mouse_entered() -> void:
-	while Input.is_action_pressed("click"):
+	#NO START HOVER IF HOLDING MOUSE
+	while Input.is_action_just_pressed("click"):
 		await get_tree().create_timer(0).timeout
 	mouse_hovering = true
-	while not Input.is_action_pressed("click"):
+	#WAIT UNTIL CLICK
+	while not Input.is_action_just_pressed("click"):
 		await get_tree().create_timer(0).timeout
+	#DRAG WINDOW
 	if mouse_hovering:
+		var start_offset : Vector2 = Vector2i(DisplayServer.mouse_get_position()) - DisplayServer.window_get_position()
 		if get_window().mode == Window.MODE_MAXIMIZED:
 			get_window().mode = Window.MODE_WINDOWED
-		var start_pos : Vector2 = get_viewport().get_mouse_position()
-		var last_mouse_pos : Vector2 = get_viewport().get_mouse_position()
-		var new_window_pos : Vector2i = get_viewport().get_mouse_position()
+		await get_tree().create_timer(0).timeout
+		var start_pos : Vector2 = DisplayServer.mouse_get_position()
+		var last_mouse_pos : Vector2 = start_pos
 		while Input.is_action_pressed("click"):
-			var difference : Vector2 = get_viewport().get_mouse_position() - last_mouse_pos
-			if ((difference.y > 0 and get_viewport().get_mouse_position().y > start_pos.y) or \
-			(difference.y < 0 and get_viewport().get_mouse_position().y < start_pos.y)):
-				new_window_pos = DisplayServer.window_get_position() + Vector2i(get_viewport().get_mouse_position() - last_mouse_pos)
-				DisplayServer.window_set_position(Vector2i(clamp(new_window_pos.x, 0, DisplayServer.screen_get_size().x), clamp(new_window_pos.y, 0, DisplayServer.screen_get_size().y)))
-			last_mouse_pos = get_viewport().get_mouse_position()
+			var mouse_pos : Vector2 = DisplayServer.mouse_get_position()
+			var distance : Vector2 = mouse_pos - last_mouse_pos
+			var current_offset : Vector2i = DisplayServer.mouse_get_position() - DisplayServer.window_get_position()
+			if (distance.y > 0 and current_offset.y < start_offset.y) \
+			or (distance.y < 0 and not current_offset.y < start_offset.y):
+				distance.y = 0
+			if (distance.x > 0 and current_offset.x < start_offset.x) \
+			or (distance.x < 0 and not current_offset.x < start_offset.x):
+				distance.x = 0
+			var final_pos : Vector2 = DisplayServer.window_get_position() + Vector2i(distance)
+			var screen_size : Vector2i = DisplayServer.screen_get_size(DisplayServer.window_get_current_screen())
+			DisplayServer.window_set_position(Vector2i(clamp(final_pos.x, 0, screen_size.x - DisplayServer.window_get_size().x), clamp(final_pos.y, 0, screen_size.y - DisplayServer.window_get_size().y)))
+			last_mouse_pos = mouse_pos
 			await get_tree().create_timer(0).timeout
+			
+		mouse_hovering = false
 
 
 func _on_control_mouse_exited() -> void:
